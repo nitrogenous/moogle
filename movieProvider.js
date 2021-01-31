0ad5e8907be0f716ddeb3016c2dc80a5051eaacf
toprakkoc <hello@toprak.dev>
2021-01-31 20:46:01 +0300

    finished the class

--- movieProvider.js
+++ movieProvider.js
@@ -13,9 +13,26 @@ const selectors = {
 };
 
 function searchForMovie() {
+  const movieName = document.getElementById(selectors.movieNameInput).value;
+  const requestUrl = encodeURI(baseUrl + movieName);
 
+  $.ajax({
+    url: requestUrl,
+    async: false,
+    dataType: 'json',
+    success: function (response) {
+      if (response.Error) {
+        updateMovieShowcase('', response.Error, '');
+        return;
+      }
+
+      updateMovieShowcase(response.Poster, response.Title, response.Plot);
+    },
+  });
 };
 
 function updateMovieShowcase(moviePoster, movieName, moviePlot) {
- 
+  document.getElementById(selectors.moviePoster).src = moviePoster;
+  document.getElementById(selectors.movieName).innerHTML = movieName;
+  document.getElementById(selectors.moviePlot).innerHTML = moviePlot;
 };
