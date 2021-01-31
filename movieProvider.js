be46f4f72369f61f1710f819eaca374cf0e69e7e
toprakkoc <hello@toprak.dev>
2021-01-31 17:07:38 +0300

    Added backup api keys

--- movieProvider.js
+++ movieProvider.js
@@ -1,4 +1,9 @@
-const apiKey = '976d21e3';
+/*
+  Programın çalışmaması durumunda apiKey değişkeninin değerini 
+  aşağıda bulunan değerlerden birisi ile değiştirip tekrar deneyin
+  6d5356a6, b65fc376, 8fe690, bb2e699b, 29089fd7
+*/
+const apiKey = '976d21e3'; 
 const baseUrl = 'https://www.omdbapi.com/?apikey=' + apiKey + '&t=';
 const selectors = {
   movieNameInput: 'movieNameInput',
