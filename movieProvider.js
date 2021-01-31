cba5b21fd6a2bf232e8163dab1989e99f3dd3331
toprakkoc <hello@toprak.dev>
2021-01-31 16:04:57 +0300

    Initial Commit

---
+++ movieProvider.js
@@ -0,0 +1,16 @@
+const apiKey = '976d21e3';
+const baseUrl = 'https://www.omdbapi.com/?apikey=' + apiKey + '&t=';
+const selectors = {
+  movieNameInput: 'movieNameInput',
+  moviePoster: 'moviePoster',
+  movieName: 'movieName',
+  moviePlot: 'moviePlot',
+};
+
+function searchForMovie() {
+
+};
+
+function updateMovieShowcase(moviePoster, movieName, moviePlot) {
+ 
+};
